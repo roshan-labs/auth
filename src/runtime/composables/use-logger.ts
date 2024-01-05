d9e5d225cef5c84aabd4869166767644c39e77a3
XiangGao <392041299@qq.com>
2024-01-05 10:08:33 +0800

    feat: @roshan-labs/auth 模块发布

--- src/runtime/composables/use-logger.ts
+++ src/runtime/composables/use-logger.ts
@@ -1,8 +1,12 @@
 export const useLogger = () => {
   return {
+    // eslint-disable-next-line no-console
     log: console.log,
+    // eslint-disable-next-line no-console
     info: console.info,
+    // eslint-disable-next-line no-console
     error: console.error,
+    // eslint-disable-next-line no-console
     warn: console.warn,
   }
 }
